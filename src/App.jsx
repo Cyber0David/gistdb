f019a213f3b379ab5afe8066ec13a0786da24fb2
Cyber0David <suchkov1david@gmail.com>
2026-06-15 21:58:53 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import HomePage from './pages/HomePage';
 import DBPage from './pages/DBPage';
+import AuthPage from './pages/AuthPage';
+import UserSettingsPage from './pages/UserSettingsPage';
 import './index.css';
 
 export default function App() {
@@ -11,6 +13,8 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/db/:id" element={<DBPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/settings" element={<UserSettingsPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
